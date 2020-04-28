0cad408eb42c3dc97f53dcc288e2519275ce851e
Jonathan Hacker <raveberry@jhacker.de>
2020-04-28 10:37:53 +0200

    manual install needed, as package repository has wrong version

--- setup/install.sh
+++ setup/install.sh
@@ -64,19 +64,28 @@ fi
 
 if [ ! -z "$AUDIO_NORMALIZATION" ] && ! type rganalysis > /dev/null 2>&1 ; then
 	echo "*** Installing rganalysis ***"
-    apt-get install -y python3-dev make faad
+	apt-get install -y python3-dev bzip2 gcc make
 
-    cd /opt
-    wget https://downloads.sourceforge.net/project/audiotools/audiotools/3.1.1/audiotools-3.1.1.tar.gz
-    tar -xf audiotools-3.1.1.tar.gz
-    rm audiotools-3.1.1.tar.gz
-    cd audiotools-3.1.1
-    python3 setup.py build
-    python3 setup.py install
+	cd /opt
+	wget https://downloads.sourceforge.net/project/audiotools/audiotools/3.1.1/audiotools-3.1.1.tar.gz
+	tar -xf audiotools-3.1.1.tar.gz
+	rm audiotools-3.1.1.tar.gz
+	cd audiotools-3.1.1
+	python3 setup.py build
+	python3 setup.py install
 
-    sudo -H pip3 install https://github.com/DarwinAwardWinner/rganalysis/archive/master.zip
+	# install faad to analyze aac files
+	cd /opt
+	wget http://downloads.sourceforge.net/sourceforge/faac/faad2-2.7.tar.bz2
+	tar -jxf faad2-2.7.tar.bz2
+	rm faad2-2.7.tar.bz2
+	cd faad2-2.7
+	./configure
+	make install
 
-    cd $SERVER_ROOT
+	sudo -H pip3 install https://github.com/DarwinAwardWinner/rganalysis/archive/master.zip
+
+	cd $SERVER_ROOT
 fi
 
 echo "*** Installing System Scripts ***"

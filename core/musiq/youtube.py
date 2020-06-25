abde5bdc068b14844700ba760c3158028fa2f330
Reinier Schoof <rschoof@transip.nl>
2020-06-25 08:46:11 +0200

    backported disabling EmbedThumbnail

--- core/musiq/youtube.py
+++ core/musiq/youtube.py
@@ -98,6 +98,11 @@ class Youtube:
             "default_search": "auto",
             "postprocessors": [
                 {"key": "FFmpegMetadata"},
+                # {
+                #     "key": "EmbedThumbnail",
+                #     # overwrite any thumbnails already present
+                #     "already_have_thumbnail": True,
+                # },
             ],
             "logger": YoutubeDLLogger(),
         }
